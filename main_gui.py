98fdbf6d2975bc01a8850b2302f47a389bf4b026
doosik0107 <33398378+doosik0107@users.noreply.github.com>
2017-12-30 22:23:13 +0900

    1. pyqt5를 이용해서 800,600짜리 창하나 띄움 2. wx를 이용해서 컴퓨터해상도값 얻어와서 가운데에 나오게함 3. 검색어부분 위젯달아놓음

---
+++ main_gui.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtWidgets import *
+import sys
+import wx
+
+class Main_Gui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        print(wx.GetDisplaySize())
+        back_size = wx.GetDisplaySize()
+        back_width = back_size[0]
+        back_height = back_size[1]
+        self.setGeometry((back_width/2)-400,(back_height/2)-300,800,600)
+        #gui창크기
+        self.setWindowTitle('travel')
+
+        self.Search_label = QLabel("검색어 : ",self)
+        self.Search_label.resize(50,30)
+        self.Search_label.move(10,20)
+
+        self.Search_inputbox = QLineEdit(self)
+        self.Search_inputbox.move(60,20)
+        self.Search_inputbox.resize(150,30)
+
+        self.Search_button = QPushButton("검색",self)
+        self.Search_button.move(220,20)
+        self.Search_button.resize(60,30)
+        self.show()
+
+
+
+if __name__=='__main__':
+    wxapp = wx.App(False)
+    app=QApplication(sys.argv)
+    travel_app = Main_Gui()
+    sys.exit(app.exec_())
